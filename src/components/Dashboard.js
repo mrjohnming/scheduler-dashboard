059f8067876c06595c29929b538f06b04fafcad6
mrjohnming <john.ngai@hotmail.com>
2022-03-04 10:18:12 -0500

    Create mock data

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,6 +2,30 @@ import React, { Component } from "react";
 import classnames from "classnames";
 import Loading from "components/Loading";
 
+// Mock data:
+const data = [
+  {
+    id: 1,
+    label: "Total Interviews",
+    value: 6
+  },
+  {
+    id: 2,
+    label: "Least Popular Time Slot",
+    value: "1pm"
+  },
+  {
+    id: 3,
+    label: "Most Popular Day",
+    value: "Wednesday"
+  },
+  {
+    id: 4,
+    label: "Interviews Per Day",
+    value: "2.3"
+  }
+];
+
 class Dashboard extends Component {
   state = { loading: false }
 
